92c33fe4e20bc725521196d0a74c97ccd902cb6c
sdeb741 <sdeb938@gmail.com>
2021-06-10 00:22:21 +0530

    translation

--- app.js
+++ app.js
@@ -2,20 +2,30 @@ var btnTranslate = document.querySelector("#btn-Translate");
 var txtInput = document.querySelector("#txt-Input");
 var outputDiv= document.querySelector("#Output");
 
+var serverURL="https://api.funtranslations.com/translate/minion.json"
 
+function getTranslationURL(text)
+{
+    return serverURL + "?" + "text=" +text;
+}
 
-outputDiv.innerText="Sayan Deb";
-
-
-console.log(outputDiv);
+function errorHandler(error)
+{
+    console.log("Error occured", error);
+    alert("Something wrong with server");
+}
 
 
 
 
 function clickHandler()
 {
-    console.log("clicked");
-    console.log("input",txtInput.value);
+    var inputText=txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response=>response.json())
+        .then(json=> console.log(json.contents.translated))
+        .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click",clickHandler);
